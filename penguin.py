2bc8df3b6158362a1b937170fa57e25c8be5a76d
Chester Kuttner <c.g.kuttner@gmail.com>
2023-04-17 18:56:18 +0100

    implemented notes and made a few class changes pendulumn now works

--- penguin.py
+++ penguin.py
@@ -7,50 +7,58 @@ pygame.init()
 screenWidth = 1280
 screenHeight = 720
 pendulumnLen = 100
+pendulumnMass = 1
 
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 clock = pygame.time.Clock()
 running = True
 
 
-class circle:
-    def __init__(self) -> None:
-        self.x = screenWidth/2
-        self.y = screenHeight/2 - pendulumnLen
+class pendulumn:
+    def __init__(self, mass, pendulumnLen) -> None:
+        self.angle = math.radians(90)
         self.colour = (15, 66, 51)
         self.radius = 10
         self.width = 4
-        self.angularAcc= 0
+        self.angularAcc = 0
+        self.netTorque = 0
+        self.mass = mass
+        self.pendulumnLen = pendulumnLen
 
-    def draw(self):
+    def draw(self, point):
         pygame.draw.circle(surface=screen, color=self.colour,
-                           width=self.width, radius=self.radius, center=(int(self.x), int(self.y)))
+                           width=self.width, radius=self.radius, center=self.getPos(point=point))
+        pygame.draw.line(surface=screen, color=(247, 152, 98),
+                         start_pos=point, end_pos=myPendulumn.getPos(point), width=3)
 
-    def setPos(self, newX, newY):
-        self.x = newX
-        self.y = newY
+    def getPos(self, point):
+        x = int(self.pendulumnLen * math.cos(self.angle) + point[0])
+        y = int(point[1]-self.pendulumnLen * math.sin(self.angle))
+        return x, y
 
-    def getPos(self):
-        return self.x, self.y
+    # def shiftDist(self, x, y):
+    #     self.x = self.x+x
+    #     self.y = self.y+y
 
-    def shiftDist(self, x, y):
-        self.x = self.x+x
-        self.y = self.y+y
+    def setAngle(self, angle):
+        self.angle = angle
 
-    def setAngle(self, point, angle):
-        
-        self.x = pendulumnLen*math.cos(angle)+point[0]     
-        self.y = pendulumnLen*math.sin(angle)+point[1]
+    def getAngle(self):
+        return self.angle
 
-    def getAngle(self,point):
-        return math.acos((self.x-point[0])/pendulumnLen)
-        
+    def applyForce(self, magnitude, direction):
+        self.netTorque += pendulumnLen * \
+            magnitude * math.sin(direction-self.angle)
 
-    def applyForce (self,magnitude, direction):
-        vertical = magnitude*math.sin(direction)
-        horizontal = magnitude*math.cos(direction)
+    def getTorque(self):
+        return self.netTorque
 
-        
+    def applyTorque(self, magnitude):
+        self.netTorque += magnitude
+
+    def move(self):
+
+        self.angle = self.netTorque / self.mass + self.angle
 
 
 class platform:
@@ -71,20 +79,21 @@ class platform:
         centerY = self.y + self.height/2
         return centerX, centerY
 
+    def move(self, speed):
+        self.x += speed
 
-myCirc = circle()
+
+myPendulumn = pendulumn(mass=2, pendulumnLen=100)
 myPlat = platform()
 
 
 def drawAll():
     screen.fill((93, 63, 211))
-    myCirc.draw()
+    myPendulumn.draw(point=myPlat.getPos())
     myPlat.draw()
-    pygame.draw.line(surface=screen, color=(247, 152, 98),
-                     start_pos=myPlat.getPos(), end_pos=myCirc.getPos(), width=3)
 
-myCirc.setAngle(point=myPlat.getPos(), angle = math.radians(5))
-print(math.degrees(myCirc.getAngle(myPlat.getPos())))
+
+myPendulumn.setAngle(math.radians(85))
 
 while running:
     # poll for events
@@ -94,17 +103,32 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
+
+    # myPlat.move(1)
+    # myPendulumn.applyForce(0.0001, math.radians(0))
+
+    # gravity
+    myPendulumn.applyForce(0.0001, math.radians(270))
+
+    # resistance of bearing
+    netTangentileForce = myPendulumn.getTorque()
+    if netTangentileForce < 0:
+        resistance = 0.00005
+    else:
+        resistance = -0.00005
+
+    if abs(netTangentileForce) < abs(resistance):
+        myPendulumn.applyTorque(-netTangentileForce)
+    else:
+        myPendulumn.applyTorque(resistance)
+
+    myPendulumn.move()
     drawAll()
-
-    
-
-    # myCirc.force(magnitude=3,direction=math.radians(185))
-
 
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
 
-    time.sleep(0.1)
+    time.sleep(0.001)
 
 pygame.quit()
